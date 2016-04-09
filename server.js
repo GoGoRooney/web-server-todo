1c6f58a78c315c1319c84cf3645c6371385d6616
Richard Moore <richmoore@bluewin.ch>
2016-04-09 15:47:49 +0200

    todo

--- server.js
+++ server.js
@@ -25,27 +25,27 @@ app.get('/', function (req,res) {
 
 // GET /todos
 app.get('/todos', function(req,res) {
-	var queryParams = req.query;
+	var query = req.query;
+	var where = {};
 
-	var filteredTodos = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false})
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
-
-
 	
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos =_.filter(filteredTodos, function(str) { 
-				return str.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) != -1;
-			 });
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
+	db.todo.findAll({where: where}).then (function(todos) {
+		res.json(todos);
+	}, function (e) {
+		 res.status(500).send();
+	});
 
-
-	res.json(filteredTodos);
 });
 
 
@@ -62,7 +62,7 @@ app.get('/todos/:id', function(req,res) {
 				res.status(404).send();
 			}
 	}, function (e) {
-		res.status(50).send();
+		res.status(500).send();
 	});
 
 });
